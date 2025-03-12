d65d4c6bc4be5be8e791cc55e9ef00f2b2e87f8f
Shio703 <shiodjamurashvili26@gmail.com>
2025-03-12 12:18:46 +0400

    .

--- jsnadPrep/tasksWithTimer2/streams.js
+++ jsnadPrep/tasksWithTimer2/streams.js
@@ -4,3 +4,14 @@
 // Create a readable stream from a large text file.
 // Pipe it into a writable stream that converts all text to uppercase and saves it to a new file.
 // Ensure efficient memory usage with highWaterMark
+const fs = require("fs");
+
+const readStream = fs.createReadStream("./bigfile.txt", { encoding: "utf-8", highWaterMark: 64 * (1024 * 1024) });
+const writable = fs.createWriteStream("./neFile.txt", { encoding: "utf-8", highWaterMark: 64 * (1024 * 1024) });
+
+readStream.on("error", (err) => console.log(err));
+readStream.on("data", (data) => {
+    writable.write(data.toUpperCase());
+});
+
+writable.on("error", (err)=> console.log(err))
--- jsnadPrep/tasksWithTimer2/timers.js
+++ jsnadPrep/tasksWithTimer2/timers.js
@@ -4,3 +4,16 @@
 // Create a function that logs "Tick!" every 1 second using setInterval().
 // After 5 ticks, stop the interval using clearInterval().
 // Before stopping, log "Timer stopped!".
+
+const myTicker = () => {
+  let ticks = 1;
+  const ticker = setInterval(() => {
+    if (ticks === 5) {
+      clearInterval(ticker);
+    }
+    console.log("tick!");
+    console.log(ticks);
+    ticks++;
+  }, 1000);
+};
+myTicker();
