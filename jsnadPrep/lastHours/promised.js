3914ad631352949468abf704d1c8bb12dda9f97e
Shio703 <shiodjamurashvili26@gmail.com>
2025-03-18 21:08:41 +0400

    added simple example of promisify method

--- jsnadPrep/lastHours/promised.js
+++ jsnadPrep/lastHours/promised.js
@@ -3,7 +3,7 @@ const fs = require("fs/promises");
 console.log("fisrt log");
 try {
     async function reader() {
-        const fileCont = await fs.readFile("./justAFile.txt",{encoding:"utf-8"});
+        const fileCont = await fs.readFile("./justAFile.txt", { encoding:"utf-8" });
         console.log(fileCont);
         console.log("second log");
     }
@@ -18,3 +18,13 @@ console.log("last log");
 // last log
 // (file content)
 // second log.
+
+// Using built in method for converting function into promisified version.
+const { readFile } = require("node:fs");
+const util = require("util");
+
+const promisifiedReader = util.promisify(readFile);
+
+promisifiedReader("./justAFile.txt", { encoding: "utf-8" }).then((res) => {
+    console.log(res);
+})
