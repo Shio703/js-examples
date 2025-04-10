7b0eec63cee2dbf6224cb11b5df1e1fe6ef81100
Shio703 <shiodjamurashvili26@gmail.com>
2025-04-10 16:43:13 +0400

    task 1.1 done

--- jsnadPrep/newTasks/1.1/answer.js
+++ jsnadPrep/newTasks/1.1/answer.js
@@ -1,10 +1,11 @@
-const fetch = require("node-fetch");
-
 async function fetchUserData() {
   // Existing code: Do not modify
   const url = "https://jsonplaceholder.typicode.com/users";
 
   // Add the missing logic here (fetch data, process the response)
+  const data = await fetch(url);
+  const toJson = await data.json();
+  return toJson.map((user) => user.username);
 }
 
 fetchUserData().then(console.log).catch(console.error);
