35debe9279f8fe6243925df5ef8c85bf157b4e55
Shio703 <shiodjamurashvili26@gmail.com>
2025-04-10 15:59:23 +0400

    task 1.0 done

--- jsnadPrep/newTasks/1.0/answer.js
+++ jsnadPrep/newTasks/1.0/answer.js
@@ -6,6 +6,17 @@ async function processFile() {
   const outputFile = "output.txt";
 
   // Add the necessary steps here (read the file, modify content, write it)
-}
+  try {
+    const fileContent = await fs.readFile(inputFile, { encoding: "utf-8" });
+    // this way we can tranform the data into uppercase,
+    // but better approach is to use streams for large files.
+    const toUpper = fileContent.toUpperCase();
 
+    await fs.writeFile(outputFile, toUpper);
+
+    console.log("Output Written!");
+  } catch (error) {
+    throw error;
+  }
+}
 processFile().catch(console.error);
