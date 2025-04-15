e51931c017043550fddde42c0d7068ce0c7dbad8
Shio703 <shiodjamurashvili26@gmail.com>
2025-04-15 17:06:24 +0400

    task 1.4 done

--- jsnadPrep/newTasks/1.4/answer.js
+++ jsnadPrep/newTasks/1.4/answer.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const fs = require("fs").promises;
 
 async function getTextFiles() {
@@ -5,6 +6,8 @@ async function getTextFiles() {
   const files = await fs.readdir(".");
 
   // Add the necessary logic here to filter `.txt` files
+  console.log("Original array: ", files);
+  return files.filter((file) => path.extname(file) === ".txt");
 }
 
 getTextFiles().then(console.log).catch(console.error);
