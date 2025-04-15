18678e141202a5eb3febae13104bf9d3ca8c4ffb
Shio703 <shiodjamurashvili26@gmail.com>
2025-04-15 16:12:49 +0400

    task 1.3 done

--- jsnadPrep/newTasks/1.3/answer.js
+++ jsnadPrep/newTasks/1.3/answer.js
@@ -5,6 +5,18 @@ async function setupEmitter() {
   const emitter = new EventEmitter();
 
   // You need to add the listener and event emitting logic here
+  console.time("total time");
+  
+  emitter.on("event", (arg) => {
+    setTimeout(() => {
+      console.log("arguments: ", arg);
+      console.timeEnd("total time");
+    }, 1000);
+  });
+
+  setTimeout(() => {
+    emitter.emit("event", "test arg");
+  }, 1000);
 }
 
 setupEmitter();
