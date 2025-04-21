21971bd8213239bb3f26fec60ca8559ee26e3c72
Shio703 <shiodjamurashvili26@gmail.com>
2025-04-21 23:09:30 +0400

    Task 1.0, 1.1 done

--- jsnadPrep/newTasks2/Task 1.1/answer.js
+++ jsnadPrep/newTasks2/Task 1.1/answer.js
@@ -1,7 +1,9 @@
+const { Buffer } = require("node:buffer");
 function toHexBuffer(inputStr) {
   if (typeof inputStr !== "string") throw new Error("Input must be string");
 
   // Implement conversion logic
+  return Buffer.from(inputStr);
 }
 
 // Example
