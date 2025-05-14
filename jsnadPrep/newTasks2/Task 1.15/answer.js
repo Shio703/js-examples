f9d10b0548be1e11f6da8f5b7307105a523bada9
Shio703 <shiodjamurashvili26@gmail.com>
2025-05-14 23:52:17 +0400

    Task 1.15 done (transform stream)

--- jsnadPrep/newTasks2/Task 1.15/answer.js
+++ jsnadPrep/newTasks2/Task 1.15/answer.js
@@ -5,6 +5,25 @@ function streamUppercase(inputFile, outputFile) {
   if (!inputFile || !outputFile) throw new Error("Both paths required");
 
   // Implement streaming logic with a Transform stream
+  const reader = fs.createReadStream(inputFile, { encoding: "utf-8" });
+
+  const transform = new Transform({
+    transform(chunk, encoding, callback) {
+      const toUpper = chunk.toString().toUpperCase();
+      callback(null, toUpper);
+    },
+  });
+
+  const writer = fs.createWriteStream(outputFile, { encoding: "utf-8" });
+
+  reader.on("error", (err) => {
+    return console.error("reader:", err);
+  });
+  transform.on("error", (err) => {
+    return console.error("transformer:", err);
+  });
+
+  reader.pipe(transform).pipe(writer);
 }
 
 // Example
