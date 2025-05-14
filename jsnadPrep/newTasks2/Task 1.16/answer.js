e4b6c2b5bb65dd0c36097492e0cdf037f50b7449
Shio703 <shiodjamurashvili26@gmail.com>
2025-05-14 14:26:19 +0400

    Task 1.16 done

--- jsnadPrep/newTasks2/Task 1.16/answer.js
+++ jsnadPrep/newTasks2/Task 1.16/answer.js
@@ -4,7 +4,18 @@ async function retryAsync(fn, retries) {
   }
 
   // Retry logic with async/await
+  for (let i = 0; i < retries; i++) {
+    try {
+      console.log(i, "from", retries);
+      return await fn();
+    } catch (error) {
+      if (i === retries - 1) {
+        throw error;
+      }
+    }
+  }
 }
-
 // Example
-retryAsync(() => fetch("http://..."), 3).then(console.log);
+retryAsync(() => fetch("https://example.co"), 3)
+  .then(console.log)
+  .catch(console.error);
