5e92a93185e5aa41e3941cc869c67bdaf5a1569d
Shio703 <shiodjamurashvili26@gmail.com>
2025-08-31 18:32:12 +0400

    fetching example using built in Fetch API++

--- jsnadPrep/tasksWithTimer/npm/task2.js
+++ jsnadPrep/tasksWithTimer/npm/task2.js
@@ -4,13 +4,26 @@
 
 const axios = require("axios");
 
-// axios
-//   .get("https://jsonplaceholder.typicode.com/posts/1")
-//   .then((response) => {
-//     console.log(response.data);
-//   });
+axios.get("https://jsonplaceholder.typicode.com/posts/1").then((response) => {
+  console.log(response.data);
+});
 
+axios
+  .post("https://jsonplaceholder.typicode.com/posts", {
+    name: "feria",
+  })
+  .then((res) => console.log(res.data));
 
-//   axios.post("https://jsonplaceholder.typicode.com/posts", {
-//     name: "feria"
-//   }).then((res)=> console.log(res.data));
+// Fetch API example using async/await:
+const fetcher = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name: "feria" }),
+  });
+  return await res.json();
+};
+fetcher().then(console.log);
+
+// this logs true. because request is instance of Object
+console.log(Request instanceof Object);
