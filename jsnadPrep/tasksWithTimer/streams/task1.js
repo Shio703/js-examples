272818358e0e95f3e2bb1fcd51bc5a856715ef8b
Shio703 <shiodjamurashvili26@gmail.com>
2025-09-03 12:02:43 +0400

    done some tasks before exam 3.9.2025

--- jsnadPrep/tasksWithTimer/streams/task1.js
+++ jsnadPrep/tasksWithTimer/streams/task1.js
@@ -1,3 +1,8 @@
 // Task 1: Read a file using streams
 // Problem: Write a script that reads bigfile.txt 
 // using a readable stream and logs the content without loading it all into memory.
+const fs = require('fs')
+
+const readStream = fs.createReadStream('./bigfile.txt', {encoding: "utf-8"});
+
+readStream.on('data', (data)=> console.log(data));
